391d66de78cfd8faf5eda0365922b677972256f7
nycbasic <samuel.poon@baruchmail.cuny.edu>
2023-01-30 22:09:59 -0500

    created signup form with bootstrap UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import SignUp from "./SignUp";
 
 function App() {
-  return <div className="App">HELLO WORLD!</div>;
+  return <div className="App"><SignUp /></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import "./index.css";
 import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.min.css";
 import reducers from "./reducers";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
