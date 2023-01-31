3c416a56bbeaa51a21c6baf30b5f28cf86a2b52d
nycbasic <samuel.poon@baruchmail.cuny.edu>
2023-01-30 21:52:37 -0500

    added all necessary npm dependancies, app setup complete

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import "./index.css";
-import App from "./App";
+import App from "./components/App";
+import reducers from "./reducers";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = createStore(reducers, applyMiddleware(thunk));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
